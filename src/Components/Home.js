8282c9d7f7a700b676556225553ea81a2f365178
parabsunita <67011382+parabsunita@users.noreply.github.com>
2024-03-25 01:08:11 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Table,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Paper,
-} from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Box } from "@mui/material";
 import axiosService from "../shared/axiosService";
 
 const Home = () => {
